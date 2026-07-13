9e3f6e5c96a3618d2ec7ea1272847cfdbcc9f511
kode0x <kode0x.work@gmail.com>
2026-07-13 12:50:16 +0530

    Algorithms

--- Algorithms/Searching Algorithms/Improved_Linear_Search.cpp
+++ Algorithms/Searching Algorithms/Improved_Linear_Search.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int linearSearch(vector<int>& arr, int key) {
     for (int index = 0; index < arr.size(); index++) {
         if (arr[index] == key) {
-            if (index > 0)
-                swap(arr[index], arr[index - 1]);
-            return index - (index > 0 ? 1 : 0);
+            if (index > 0) {
+                swap(arr[index], arr[index - 1]); // Transposition
+            }
+            // Another Method: Move To Front/Head
+            // swap(arr[index], arr[0]);
+            return (index > 0) ? index - 1 : index;
         }
     }
     return -1;
--- Algorithms/Searching Algorithms/Linear_Search.cpp
+++ Algorithms/Searching Algorithms/Linear_Search.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int linearSearch(const vector<int>& arr, int key) {
     for (int index = 0; index < arr.size(); index++) {
         if (arr[index] == key)
